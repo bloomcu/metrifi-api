65c6b256052e55889666f002882655326002c77a
Ryan Harmon <ryan@bloomcu.com>
2024-01-31 12:11:02 -0700

    Add funnel request and update method

---
+++ app/Domain/Funnels/Requests/FunnelUpdateRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DDD\Domain\Funnels\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class FunnelUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'connection_id' => 'nullable|numeric',
+            'name' => 'nullable|string',
+            'description' => 'nullable|string',
+        ];
+    }
+
+    /**
+     * Return exception as json
+     *
+     * @return Exception
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'The given data was invalid.',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
