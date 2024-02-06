20ac8673fc4d9dfd542016520623fb0bbe6c0e26
Ryan Harmon <ryan@bloomcu.com>
2024-02-06 11:30:50 -0700

    Replace type column with metric and expression with measurables.

---
+++ database/migrations/2024_02_06_010131_update_funnel_steps_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('funnel_steps', function (Blueprint $table) {
+            $table->text('metric')->after('type')->nullable();
+            $table->dropColumn('type');
+
+            $table->json('measurables')->after('expression')->nullable();
+            $table->dropColumn('expression');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('funnel_steps', function (Blueprint $table) {
+            $table->json('type')->after('metric')->nullable();
+            $table->dropColumn('metric');
+
+            $table->json('expression')->after('measurables')->nullable();
+            $table->dropColumn('measurables');
+        });
+    }
+};
