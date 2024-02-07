e0793523e06c2e0be55fac01eed8973738c790c5
Ryan Harmon <ryan@bloomcu.com>
2024-02-07 12:01:07 -0700

    Update funnel step create request

--- app/Http/Funnels/FunnelStepController.php
+++ app/Http/Funnels/FunnelStepController.php
@@ -6,20 +6,16 @@ use Illuminate\Http\Request;
 use DDD\Domain\Organizations\Organization;
 use DDD\Domain\Funnels\Resources\FunnelStepResource;
 use DDD\Domain\Funnels\Requests\StepUpdateRequest;
+use DDD\Domain\Funnels\Requests\StepCreateRequest;
 use DDD\Domain\Funnels\FunnelStep;
 use DDD\Domain\Funnels\Funnel;
 use DDD\App\Controllers\Controller;
 
 class FunnelStepController extends Controller
 {
-    public function store(Organization $organization, Funnel $funnel, Request $request)
+    public function store(Organization $organization, Funnel $funnel, StepCreateRequest $request)
     {
-        $step = $funnel->steps()->create([
-            'metric' => $request->metric,
-            'name' => $request->name,
-            'description' => $request->description,
-            'measurables' => $request->measurables,
-        ]);
+        $step = $funnel->steps()->create($request->validated());
 
         return new FunnelStepResource($step);
     }
