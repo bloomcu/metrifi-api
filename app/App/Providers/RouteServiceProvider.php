da4a58154602e204c03572595101e052b2d74dce
Ryan Harmon <ryan@bloomcu.com>
2024-02-13 22:09:45 -0700

    Increase request rate limits

--- app/App/Providers/RouteServiceProvider.php
+++ app/App/Providers/RouteServiceProvider.php
@@ -66,7 +66,7 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(180)->by($request->user()?->id ?: $request->ip());
         });
     }
 }
