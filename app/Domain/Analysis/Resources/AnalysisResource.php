4902d4227b095c1d13860b3b05703a664bac778a
Ryan Harmon <ryan@bloomcu.com>
2024-07-02 12:34:53 -0600

    Analysis boilerplate

---
+++ app/Domain/Analysis/Resources/AnalysisResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DDD\Domain\Analysis\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use DDD\Domain\Funnels\Resources\FunnelResource;
+use DDD\Domain\Dashboards\Resources\DashboardResource;
+
+class AnalysisResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'in_progress' => $this->in_progress,
+            'content' => $this->content,
+            'subject_funnel' => new FunnelResource($this->subjectFunnel),
+            'dashboard' => new DashboardResource($this->dashboard),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
