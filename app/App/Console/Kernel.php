54c6eab07bc81ea51fda32740d82d972a638d22d
Ryan Harmon <ryan@bloomcu.com>
2024-08-09 16:49:22 -0600

    Reschedule

--- app/App/Console/Kernel.php
+++ app/App/Console/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('admin:analyze-all-dashboards')->dailyAt('02:00'); // 2:00 AM UTC (https://www.timeanddate.com/worldclock/timezone/utc)
-        $schedule->command('admin:analyze-all-dashboards')->dailyAt('4:55')->timezone('America/Denver');	; // https://www.timeanddate.com/worldclock/timezone/utc
+        $schedule->command('admin:analyze-all-dashboards')->dailyAt('17:00')->timezone('America/Denver');	; // https://www.timeanddate.com/worldclock/timezone/utc
     }
 
     /**
