8950ff11fc74b9b8e67fc9206b5426c74c3585ce
Ryan Harmon <ryan@bloomcu.com>
2024-08-09 15:03:27 -0600

    Disable scheduled command

--- app/App/Console/Kernel.php
+++ app/App/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('admin:analyze-all-dashboards')->hourlyAt(1);
+        // $schedule->command('admin:analyze-all-dashboards')->dailyAt('02:00'); // 2:00 AM UTC
     }
 
     /**
