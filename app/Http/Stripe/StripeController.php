5b09fd1f1380ffd047a1a610d8519eba1da28e2e
Ryan Harmon <ryan@bloomcu.com>
2024-10-30 14:17:17 -0600

    Cancel now

--- app/Http/Stripe/StripeController.php
+++ app/Http/Stripe/StripeController.php
@@ -11,8 +11,13 @@ class StripeController extends Controller
     public function checkout(Organization $organization, Request $request)
     {
         $session = $organization->newSubscription('default', $request->price_id)->checkout([
+            // Staging
             'success_url' => 'https://staging.metrifi.com/' . $organization->slug . '/settings/billing?success=true',
             'cancel_url' => 'https://staging.metrifi.com/' . $organization->slug . '/settings/billing?cancel=true',
+
+            // Local
+            // 'success_url' => 'http://localhost:3000/' . $organization->slug . '/settings/billing?success=true',
+            // 'cancel_url' => 'http://localhost:3000/' . $organization->slug . '/settings/billing?cancel=true',
         ]);
 
         return response()->json([
@@ -22,7 +27,7 @@ class StripeController extends Controller
 
     public function cancel(Organization $organization)
     {
-        $organization->subscription('default')->cancel();
+        $organization->subscription('default')->cancelNow();
 
         return response()->json([
             'message' => 'Subscription canceled successfully',
