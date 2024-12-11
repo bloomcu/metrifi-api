90eb09fb1008033a27b7d799ee6ce6796db8d162
Ryan Harmon <ryan@bloomcu.com>
2024-12-11 11:01:25 -0700

    Add 10s wait on screenshots

--- app/App/Services/Screenshot/Thumbio.php
+++ app/App/Services/Screenshot/Thumbio.php
@@ -18,7 +18,7 @@ class Thumbio implements ScreenshotInterface
      */
     public function getScreenshot(
         string $url, 
-        string $wait = '0', 
+        string $wait = '10', 
         string $width = '1200', 
         string $height = '1200'
     ){
