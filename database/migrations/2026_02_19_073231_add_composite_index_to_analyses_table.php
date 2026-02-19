c2280477592a3455de6f6ebc7dd392568058af13
Ryan Harmon <ryan@bloomcu.com>
2026-02-19 07:34:26 -0700

    optimize admin dashboards query

---
+++ database/migrations/2026_02_19_073231_add_composite_index_to_analyses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('analyses', function (Blueprint $table) {
+            $table->index(['dashboard_id', 'type', 'deleted_at', 'created_at'], 'analyses_dashboard_type_deleted_created_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('analyses', function (Blueprint $table) {
+            $table->dropIndex('analyses_dashboard_type_deleted_created_index');
+        });
+    }
+};
